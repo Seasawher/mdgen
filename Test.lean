66a4c9953c0ff2d733b585b8ad6002d2fc52d39d
Seasawher <tellagoodstory@icloud.com>
2024-01-06 21:36:34 +0900

    fix typo

--- Test.lean
+++ Test.lean
@@ -5,15 +5,15 @@
 -/
 
 def main : IO UInt32 := do
-  let firstActual ← IO.FS.readFile ⟨"Test/Out/first.md"⟩
-  let firstExpected ← IO.FS.readFile ⟨"Test/Exp/first.md"⟩
+  let firstActual ← IO.FS.readFile ⟨"Test/Out/First.md"⟩
+  let firstExpected ← IO.FS.readFile ⟨"Test/Exp/First.md"⟩
   if firstActual != firstExpected then
-    IO.eprintln "first.md is not as expected"
+    IO.eprintln "First.md is not as expected"
     return 1
 
-  let secondActual ← IO.FS.readFile ⟨"Test/Out/Second/second.md"⟩
-  let secondExpected ← IO.FS.readFile ⟨"Test/Exp/Second/second.md"⟩
+  let secondActual ← IO.FS.readFile ⟨"Test/Out/Second/Second.md"⟩
+  let secondExpected ← IO.FS.readFile ⟨"Test/Exp/Second/Second.md"⟩
   if secondActual != secondExpected then
-    IO.eprintln "second.md is not as expected"
+    IO.eprintln "Second.md is not as expected"
     return 1
   return 0
