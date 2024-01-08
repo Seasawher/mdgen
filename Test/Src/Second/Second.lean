6ff967b41f2e554d747b6206f0c5d26ca0ed45ee
Seasawher <tellagoodstory@icloud.com>
2024-01-08 12:21:13 +0900

    modify test case

--- Test/Src/Second/Second.lean
+++ Test/Src/Second/Second.lean
@@ -1,7 +1,1 @@
-/- # sample of doc comment and ignoring -/
-
-variable (n : Nat) --#
-
-/-- adding zero changes nothing -/
-theorem add_zero : n + 0 = n := by
-  rw [Nat.add_zero]
+/- Lean is an awesome language -/
