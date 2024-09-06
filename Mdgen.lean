2d5adf059261bcf2235da864f7d652b35eae489f
Seasawher <tellagoodstory@icloud.com>
2024-09-07 01:17:47 +0900

    use Cli tool for Lean
    resolve #32

--- Mdgen.lean
+++ Mdgen.lean
@@ -1,26 +1,5 @@
-import Mdgen.File
-import Mdgen.ConvertToMd
+import Mdgen.Cli
 
-open System
 
-def main (args : List String) : IO UInt32 := do
-  if args.length != 2 then
-    IO.eprintln s!"usage: mdgen <input_dir> <output_dir>"
-    return 1
-
-  let inputDir : FilePath := args.get! 0
-  let outputDir : FilePath := args.get! 1
-
-  let paths ← getLeanFilePaths inputDir
-
-  for path in paths do
-    let mut content ← IO.FS.lines path
-    content := content.map (fun line => line.replace "\r" "")
-
-    let outputFilePath := outputFilePath
-      inputDir.components
-      outputDir.components
-      path.components
-
-    createFile (genPath outputFilePath) (convertToMd content.toList)
-  return 0
+def main (args : List String) : IO UInt32 :=
+  mkMdgenCmd.validate args
---
+++ Mdgen/Cli.lean
@@ -0,0 +1,32 @@
+import Mdgen.File
+import Mdgen.ConvertToMd
+import Cli
+
+open Cli System
+
+def runMdgenCmd (p : Parsed) : IO UInt32 := do
+  let inputDir : FilePath := p.positionalArg! "input_dir" |>.as! String
+  let outputDir : FilePath := p.positionalArg! "output_dir" |>.as! String
+
+  let paths ← getLeanFilePaths inputDir
+
+  for path in paths do
+    let mut content ← IO.FS.lines path
+    content := content.map (fun line => line.replace "\r" "")
+
+    let outputFilePath := outputFilePath
+      inputDir.components
+      outputDir.components
+      path.components
+
+    createFile (genPath outputFilePath) (convertToMd content.toList)
+  return 0
+
+def mkMdgenCmd : Cmd := `[Cli|
+  mkMdgenCmd VIA runMdgenCmd; ["1.4.0"]
+  "mdgen is a tool to generate .md files from .lean files."
+
+  ARGS:
+    input_dir : String; "The directory containing the input Lean files."
+    output_dir : String; "The directory to write the markdown files."
+]
