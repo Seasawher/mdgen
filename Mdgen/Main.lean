2c2a52078b49bd16c9f1c08499615d29c618ecb0
Seasawher <tellagoodstory@icloud.com>
2024-09-07 01:08:10 +0900

    move functions

--- Mdgen/Main.lean
+++ Mdgen/Main.lean
@@ -3,20 +3,6 @@ import Mdgen.ConvertToMd
 
 open System
 
-private partial def getLeanFilePaths (fp : FilePath) (acc : Array FilePath := #[]) :
-    IO $ Array FilePath := do
-  if ← fp.isDir then
-    (← fp.readDir).foldlM (fun acc dir => getLeanFilePaths dir.path acc) acc
-  else return if fp.extension == some "lean" then acc.push fp else acc
-
-/-- create a file with given path and content. -/
-private def createFile (path : FilePath) (content : String) : IO Unit := do
-  match path.parent with
-  | none => IO.FS.writeFile path content
-  | some parent =>
-    IO.FS.createDirAll parent
-    IO.FS.writeFile path content
-
 def main (args : List String) : IO UInt32 := do
   if args.length != 2 then
     IO.eprintln s!"usage: mdgen <input_dir> <output_dir>"
