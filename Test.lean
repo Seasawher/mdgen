c31277771fcae25ba77758d28e8d993456554089
Seasawher <tellagoodstory@icloud.com>
2025-02-09 14:50:48 +0900

    refactor `Test.lean`

--- Test.lean
+++ Test.lean
@@ -1,12 +1,11 @@
 def main : IO UInt32 := do
   let testTargets := ["First.md", "Second/Second.md"]
 
-  let mut failed := false
   for test_file in testTargets do
-    let firstActual ← IO.FS.readFile ⟨s!"Test/Out/{test_file}"⟩
-    let firstExpected ← IO.FS.readFile ⟨s!"Test/Exp/{test_file}"⟩
-    if firstActual != firstExpected then
+    let actual ← IO.FS.readFile ⟨s!"Test/Out/{test_file}"⟩
+    let expected ← IO.FS.readFile ⟨s!"Test/Exp/{test_file}"⟩
+    if actual != expected then
       IO.println s!"error: {test_file} is not as expected"
-      failed := true
+      return 1
 
-  return if failed then 1 else 0
+  return 0
