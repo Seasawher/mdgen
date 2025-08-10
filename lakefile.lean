c2fa7e909ca73ff684a744df46e93751e6b8cb83
Seasawher <tellagoodstory@icloud.com>
2025-08-11 06:20:16 +0900

    update version string

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 package «mdgen» where
-  version := v!"1.11.0"
+  version := v!"2.0.0"
   keywords := #["cli", "markdown"]
   description := "Tool to generate markdown files from lean files."
 
