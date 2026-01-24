8e0524310148337b54602f6f1b30578ee2910106
Seasawher <tellagoodstory@icloud.com>
2026-01-24 22:40:03 +0900

    update to `v4.27.0`

--- Mdgen/Cli.lean
+++ Mdgen/Cli.lean
@@ -43,7 +43,7 @@ public def runMdgenCmd (p : Parsed) : IO UInt32 := do
 
 /-- API definition of `mdgen` command -/
 public def mkMdgenCmd : Cmd := `[Cli|
-  mdgen VIA runMdgenCmd; ["v4.27.0-rc1"]
+  mdgen VIA runMdgenCmd; ["v4.27.0"]
   "mdgen is a tool to generate .md files from .lean files."
 
   FLAGS:
